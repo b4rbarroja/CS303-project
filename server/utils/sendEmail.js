9a4bf2f468945a6c831322646cc5e0328a9d2dfd
abdelfattahmostafa <abdelfattahmostafa102@gmail.com>
2026-05-08 16:40:58 +0300

    Update sendEmail.js

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,13 +2,10 @@ import nodeMailer from "nodemailer";
 
 export const sendEmail = async ({ email, subject, message }) => {
   const transporter = nodeMailer.createTransport({
-    host: process.env.SMTP_HOST,
-    service: process.env.SMTP_SERVICE,
-    port: process.env.SMTP_PORT,
-    secure: true, 
+    service: "gmail", // استخدام service بيظبط الـ host والـ port والـ secure تلقائياً
     auth: {
       user: process.env.SMTP_MAIL,
-      pass: process.env.SMTP_PASSWORD,
+      pass: process.env.SMTP_PASSWORD, // لازم يكون App Password من إعدادات حساب جوجل
     },
   });
 
@@ -16,9 +13,8 @@ export const sendEmail = async ({ email, subject, message }) => {
     from: process.env.SMTP_MAIL,
     to: email,
     subject,
-    html: message, 
+    html: message,
   };
 
- 
   await transporter.sendMail(mailOptions);
 };
