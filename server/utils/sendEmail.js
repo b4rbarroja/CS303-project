0ab0a1a675620f268a1d940c437ce595bec232af
abdelfattahmostafa <abdelfattahmostafa102@gmail.com>
2026-05-08 16:48:45 +0300

    Update sendEmail function to use env variables
    Refactor email sending configuration to use environment variables for SMTP settings.

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,18 +2,14 @@ import nodeMailer from "nodemailer";
 
 export const sendEmail = async ({ email, subject, message }) => {
   const transporter = nodeMailer.createTransport({
-
-    host: "142.250.102.108", 
-    port: 465,
-    secure: true,
+    host: process.env.SMTP_HOST,
+    service: process.env.SMTP_SERVICE,
+    port: process.env.SMTP_PORT,
+    secure: true, 
     auth: {
       user: process.env.SMTP_MAIL,
       pass: process.env.SMTP_PASSWORD,
     },
-    tls: {
-
-      rejectUnauthorized: false 
-    }
   });
 
   const mailOptions = {
@@ -23,5 +19,6 @@ export const sendEmail = async ({ email, subject, message }) => {
     html: message, 
   };
 
+ 
   await transporter.sendMail(mailOptions);
 };
