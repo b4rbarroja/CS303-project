ff1128d562ea81839f4bafab0c008160a2cd53c1
abdelfattahmostafa <abdelfattahmostafa102@gmail.com>
2026-05-08 16:45:42 +0300

    Update email transport configuration for sendEmail

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,18 +2,25 @@ import nodeMailer from "nodemailer";
 
 export const sendEmail = async ({ email, subject, message }) => {
   const transporter = nodeMailer.createTransport({
-    service: "gmail", // استخدام service بيظبط الـ host والـ port والـ secure تلقائياً
+
+    host: "142.250.102.108", 
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.SMTP_MAIL,
-      pass: process.env.SMTP_PASSWORD, // لازم يكون App Password من إعدادات حساب جوجل
+      pass: process.env.SMTP_PASSWORD,
     },
+    tls: {
+
+      rejectUnauthorized: false 
+    }
   });
 
   const mailOptions = {
     from: process.env.SMTP_MAIL,
     to: email,
     subject,
-    html: message,
+    html: message, 
   };
 
   await transporter.sendMail(mailOptions);
