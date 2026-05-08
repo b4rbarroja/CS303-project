5592373cde2b7d0e84ddd718d8cd96f496221362
zeyad khaled <zeyadkhaled20289@gmail.com>
2026-05-08 19:42:38 +0300

    Refactor sendEmail to use Resend service

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,24 +1,12 @@
-import nodeMailer from "nodemailer";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async ({ email, subject, message }) => {
-  const transporter = nodeMailer.createTransport({
-    host: process.env.SMTP_HOST,
-    service: process.env.SMTP_SERVICE,
-    port: process.env.SMTP_PORT,
-    secure: true, 
-    auth: {
-      user: process.env.SMTP_MAIL,
-      pass: process.env.SMTP_PASSWORD,
-    },
-  });
-
-  const mailOptions = {
-    from: process.env.SMTP_MAIL,
+  await resend.emails.send({
+    from: "E-Library <onboarding@resend.dev>",
     to: email,
     subject,
     html: message, 
-  };
-
- 
-  await transporter.sendMail(mailOptions);
+  });
 };
